dda6fd5a148dbbc3c3377747367dd2eea159d8ce
羅文森 <vincentale.21@gmail.com>
2025-09-06 22:05:02 +0800

    u

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/campB-git-practise/',
+  base: '/Yeah-Fun-Cheap/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
